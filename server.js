d6ed4aa8d12609f11a51e0412ea150bfc147127e
muza97 <mustafa.zaitoun97@gmail.com>
2023-10-11 11:47:49 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const app = express();  // <-- Definiera app här
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./myproject/config/firebaseServiceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://YOUR-PROJECT-ID.firebaseio.com"  // Ändra detta till din faktiska databas-URL
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'views')));  // <-- Flytta denna här
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
